d5ebc6db87d9314263e1bcf98c53bb7881142d4b
AhsanuQaulan <ahsanuqaulan1208@gmail.com>
2025-10-09 16:20:19 +0800

    update latihan dan tugas hari 3

---
+++ latihan/hari3/async.js
@@ -0,0 +1,16 @@
+// callback
+function periksaDokter(nomerAntri, callback) {
+  if (nomerAntri > 50) {
+    callback(false);
+  } else if (nomerAntri < 10) {
+    callback(true);
+  }
+}
+
+periksaDokter(65, function (check) {
+  if (check) {
+    console.log("sebentar lagi giliran saya");
+  } else {
+    console.log("saya jalan-jalan dulu");
+  }
+});
